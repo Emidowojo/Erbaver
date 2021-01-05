3b82fc5068e866afb91db4526bb62c0f04f31cd4
Emidowojo <54295353+Emidowojo@users.noreply.github.com>
2021-01-05 16:49:58 +0100

    Add files via upload

---
+++ Clubapp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Clubapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string theName = "Christiana";
+            int age = 0;
+            Console.Write("Hi!" + thename + how old are you?: " );
+            age = int.Parse(Console.ReadLine());
+            if(age >= 18)
+            {
+                Console.WriteLine("Welcome to Incongurous, what can i get you?: ");
+            }
+            else if (age<= 11)
+            {
+                Console.WriteLine("You shouldn't be here!");
+            }
+            else
+            {
+                Console.WriteLine("You're too young to be here, Please leave");
+            }
+
+             string herName = "Susan";
+            int age = 0;
+            Console.Write("Hi!" + hername + "how old are you?" );
+            age = int.Parse(Console.ReadLine());
+            if(age >= 18)
+            {
+                Console.WriteLine("Welcome to Incongurous, what can i get you?: ");
+            }
+            else if (age<= 11)
+            {
+                Console.WriteLine("You shouldn't be here!");
+            }
+            else
+            {
+                Console.WriteLine("You're too young to be here, Please leave");
+            }
+
+             string boysName = "Jael";
+            int age = 0;
+            Console.Write("Hi!" + boysName + "how old are you?" );
+            age = int.Parse(Console.ReadLine());
+            if(age >= 18)
+            {
+                Console.WriteLine("Welcome to Incongurous, what can i get you?: ");
+            }
+            else if (age<= 11)
+            {
+                Console.WriteLine("You shouldn't be here!");
+            }
+            else
+            {
+                Console.WriteLine("You're too young to be here, Please leave");
+            }
+        }
+    }
+}
