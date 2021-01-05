ec3026c4b9e623b827cb3ee95a929e75f83d32d5
Emidowojo <54295353+Emidowojo@users.noreply.github.com>
2021-01-05 16:05:06 +0100

    Add files via upload

---
+++ Variables3/!st Assignment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Variables3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string lotteryNo1 = "";
+             string lotteryNo2 = "";
+             string lotteryNo3 ="";
+            string result = "";
+
+            Console.Write("Enter Lottery No1: ");
+            lotteryNo1 = Console.ReadLine();
+
+
+           Console.Write("Enter Lottery No2: " );
+            lotteryNo2 = Console.ReadLine();
+
+            Console.Write("Enter Lottery No3: ");
+             lotteryNo3 =Console.ReadLine();
+
+
+        
+
+           result = lotteryNo1 + lotteryNo2 + lotteryNo3;
+           Console.WriteLine("The answer is: " + result);
+          
+            
+        }
+    }
+}
